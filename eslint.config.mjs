109fd9003cc9bc12ac02ba69143d5d09f696cde2
Momin Shaikh <mominshaikhdevs@gmail.com>
2026-07-20 00:53:39 +0600

    adding eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "no-console": "off"
+    }
+  }
+];
